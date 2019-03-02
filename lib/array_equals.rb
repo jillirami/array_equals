281dbb5d7081433646e98ae7842735b49bfaeed3
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-03-02 09:23:38 -0800

    complete assignment

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,18 +2,16 @@
 # and the same integer values in the same exact order
 
 def array_equals(array_a, array_b)
-  array_a = array_a.to_a
-  array_b = array_b.to_a
-  if (array_a.length == array_b.length)
-    array_a.each_with_index do |item1, i|
-      if array_b[i] == item1
-      else
+  if (array_a == [] && array_b == [] || array_a == nil && array_b == nil)
+    return true
+  elsif array_a == nil || array_b == nil || array_a.length != array_b.length
+    return false
+  else
+    array_a.length.times do |item|
+      if array_b[item] != array_a[item]
         return false
       end
     end
     return true
-  else
-    return false
   end
-  raise NotImplementedError
 end
