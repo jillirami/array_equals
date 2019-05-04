94d945539bc696c694b1c733db80344893948404
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-04 13:07:00 -0700

    refactored solution

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,17 +1,17 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 
-def array_equals(array_a, array_b)
-  if (array_a == [] && array_b == [] || array_a == nil && array_b == nil)
+def array_equals(ar1, ar2)
+  if !ar1 && !ar2
     return true
-  elsif array_a == nil || array_b == nil || array_a.length != array_b.length
-    return false
+  elsif !ar1 || !ar2 || ar1.length != ar2.length
+   return false
   else
-    array_a.length.times do |item|
-      if array_b[item] != array_a[item]
+    ar1.length.times do |i| 
+      if ar1[i] != ar2[i]
         return false
       end
     end
     return true
   end
-end
+ end
